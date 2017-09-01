e7aea4aa25f5aeecba57a9845d83d48aab51cc59
Carl Grimsborn <carlgrimsborn@hotmail.com>
2017-09-01 14:41:03 +0200

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by Carl Grimsborn on 2017-09-01.
+//  Copyright © 2017 Carl Grimsborn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
